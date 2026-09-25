ddb748d2d937e189b01ccdb8e2565a63b4fcb65e
Ryellacaa <cantikabojonggede@gmail.com>
2026-09-25 17:11:09 +0700

    update

--- api/index.php
+++ api/index.php
@@ -1,6 +1,10 @@
 <?php
 
-// Paksa direktori cache & storage Laravel mengarah ke /tmp serverless Vercel
+// Force HTTPS dan URL custom domain
+$_SERVER['HTTPS'] = 'on';
+$_ENV['APP_URL'] = 'https://lms.xi-rekayasa.my.id';
+
+// Storage & Cache sementara untuk Vercel
 $_ENV['APP_CONFIG_CACHE'] = '/tmp/config.php';
 $_ENV['APP_SERVICES_CACHE'] = '/tmp/services.php';
 $_ENV['APP_PACKAGES_CACHE'] = '/tmp/packages.php';
