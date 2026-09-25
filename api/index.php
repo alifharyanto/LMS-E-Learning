9cbab9235241a92185195e3839b4fb5a57585390
alifharyanto <alifharyanto201012@gmail.com>
2026-09-25 17:45:58 +0700

    force cookie session driver for vercel read-only filesystem

--- api/index.php
+++ api/index.php
@@ -5,19 +5,22 @@ if (isset($_ENV['VERCEL']) || isset($_SERVER['VERCEL'])) {
         $_SERVER['HTTPS'] = 'on';
     }
 
-    // Path cache & view sementara Vercel
+    // Paksa Session & Cache menggunakan Cookie & Array
+    $_ENV['SESSION_DRIVER'] = 'cookie';
+    $_SERVER['SESSION_DRIVER'] = 'cookie';
+    
+    $_ENV['CACHE_STORE'] = 'array';
+    $_SERVER['CACHE_STORE'] = 'array';
+
+    $_ENV['LOG_CHANNEL'] = 'stderr';
+    $_SERVER['LOG_CHANNEL'] = 'stderr';
+
+    // Path sementara Vercel
     putenv('VIEW_COMPILED_PATH=/tmp');
     putenv('APP_CONFIG_CACHE=/tmp/config.php');
     putenv('APP_SERVICES_CACHE=/tmp/services.php');
     putenv('APP_PACKAGES_CACHE=/tmp/packages.php');
     putenv('APP_ROUTES_CACHE=/tmp/routes.php');
-
-    // Alihkan log ke errorlog (stdout) agar tidak menulis file laravel.log
-    putenv('LOG_CHANNEL=stderr');
-
-    // Alihkan session dan cache ke driver cookie / array
-    putenv('SESSION_DRIVER=cookie');
-    putenv('CACHE_STORE=array');
 }
 
 require __DIR__ . '/../public/index.php';
