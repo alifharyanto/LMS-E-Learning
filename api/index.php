3c41694646e57821b10510e672655d017ecddd48
alifharyanto <alifharyanto201012@gmail.com>
2026-09-25 17:39:51 +0700

    api/index log

--- api/index.php
+++ api/index.php
@@ -1,17 +1,19 @@
 <?php
 
-// Force HTTPS
-if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
-    $_SERVER['HTTPS'] = 'on';
-}
+if (isset($_ENV['VERCEL']) || isset($_SERVER['VERCEL'])) {
+    if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
+        $_SERVER['HTTPS'] = 'on';
+    }
 
-// Set storage & cache path ke /tmp Vercel
-$_ENV['APP_CONFIG_CACHE'] = '/tmp/config.php';
-$_ENV['APP_SERVICES_CACHE'] = '/tmp/services.php';
-$_ENV['APP_PACKAGES_CACHE'] = '/tmp/packages.php';
-$_ENV['APP_ROUTES_CACHE'] = '/tmp/routes.php';
-$_ENV['VIEW_COMPILED_PATH'] = '/tmp';
-$_ENV['CACHE_STORE'] = 'array';
-$_ENV['SESSION_DRIVER'] = 'cookie';
+    // Set path cache sementara
+    putenv('VIEW_COMPILED_PATH=/tmp');
+    putenv('APP_CONFIG_CACHE=/tmp/config.php');
+    putenv('APP_SERVICES_CACHE=/tmp/services.php');
+    putenv('APP_PACKAGES_CACHE=/tmp/packages.php');
+    putenv('APP_ROUTES_CACHE=/tmp/routes.php');
+
+    // Hapus file compiled view lama di /tmp jika ada perbaikan UI baru
+    array_map('unlink', glob("/tmp/*.php"));
+}
 
 require __DIR__ . '/../public/index.php';
