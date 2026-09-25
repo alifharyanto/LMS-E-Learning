26ca1fd0b97eeedae70a8cd7284d416028ca5a20
alifharyanto <alifharyanto201012@gmail.com>
2026-09-25 16:32:47 +0700

    fix vercel temporary storage path

--- api/index.php
+++ api/index.php
@@ -1,3 +1,12 @@
 <?php
 
+// Paksa direktori cache & storage Laravel mengarah ke /tmp serverless Vercel
+$_ENV['APP_CONFIG_CACHE'] = '/tmp/config.php';
+$_ENV['APP_SERVICES_CACHE'] = '/tmp/services.php';
+$_ENV['APP_PACKAGES_CACHE'] = '/tmp/packages.php';
+$_ENV['APP_ROUTES_CACHE'] = '/tmp/routes.php';
+$_ENV['VIEW_COMPILED_PATH'] = '/tmp';
+$_ENV['CACHE_STORE'] = 'array';
+$_ENV['SESSION_DRIVER'] = 'cookie';
+
 require __DIR__ . '/../public/index.php';
